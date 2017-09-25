6d2bc3c5b2d172c29fecc2f6e7b0109cec36f164
Pavel Tkachuk <pavelt0309@gmail.com>
2017-09-25 21:44:26 +0300

    Implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.g = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.g = Math.round((this.max + this.min) / 2);
+    	return this.g;
     }
 
     lower() {
-
+    	this.max = this.g;
     }
 
     greater() {
-
+    	this.min = this.g;
     }
 }
 
